35329e6230e2b35adb483ecfba0b0863ff66170a
Alexander Mühlbauer <alexander.muehlbauer@gesis.org>
2020-06-29 16:23:06 +0200

    pom -> Reuse cessda vaadin css from https://bitbucket.org/cessda/cessda.cvs.gui/commits/99b3104c4395e8adf3bf371de09686b633235a92 by copy-paste

--- src/main/java/eu/cessda/cmv/server/UiView.java
+++ src/main/java/eu/cessda/cmv/server/UiView.java
@@ -2,6 +2,9 @@ package eu.cessda.cmv.server;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.vaadin.annotations.StyleSheet;
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
 import com.vaadin.navigator.Navigator;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
@@ -9,6 +12,9 @@ import com.vaadin.spring.navigator.SpringViewProvider;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
+@Title( "CESSDA Metadata Validator" )
+@StyleSheet( { "https://fonts.googleapis.com/css?family=Source+Sans+Pro:100,200,300,400,500,600,700,800,900" } )
+@Theme( "mytheme" )
 @SpringUI( path = "/ui" )
 public class UiView extends UI
 {
