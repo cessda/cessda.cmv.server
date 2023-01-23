b9268b8450ac110a030acb1234f80d1d37506e90
Matthew Morris <matthew.morris@cessda.eu>
2023-01-23 09:45:55 +0100

    licence -> Update the copyright statement in the Java source files (#91)

--- src/test/java/eu/cessda/cmv/server/ServerTest.java
+++ src/test/java/eu/cessda/cmv/server/ServerTest.java
@@ -2,14 +2,14 @@
  * #%L
  * CESSDA Metadata Validator
  * %%
- * Copyright (C) 2020 - 2021 CESSDA ERIC
+ * Copyright (C) 2020 - 2023 CESSDA ERIC
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,11 +19,11 @@
  */
 package eu.cessda.cmv.server;
 
+import org.junit.Test;
+
 import static eu.cessda.cmv.server.Server.ALLOWED_CLI_OPTION;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
-import org.junit.Test;
 
 public class ServerTest
 {
