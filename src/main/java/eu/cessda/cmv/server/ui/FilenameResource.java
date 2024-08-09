42a53e623185759e51ce91b2b17379acc6c36dd4
Matthew Morris <matthew.morris@cessda.eu>
2024-08-09 14:48:23 +0200

    ui → add equals and hashcode to FilenameResource

--- src/main/java/eu/cessda/cmv/server/ui/FilenameResource.java
+++ src/main/java/eu/cessda/cmv/server/ui/FilenameResource.java
@@ -9,9 +9,9 @@ package eu.cessda.cmv.server.ui;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,8 @@ package eu.cessda.cmv.server.ui;
  */
 
 import org.springframework.core.io.ByteArrayResource;
+
+import java.util.Objects;
 
 public class FilenameResource extends ByteArrayResource
 {
@@ -42,5 +44,20 @@ public class FilenameResource extends ByteArrayResource
 	public String getDescription()
 	{
 		return "File name resource [" + this.fileName + "]";
+	}
+
+	@Override
+	public boolean equals( Object o )
+	{
+		if ( this == o ) return true;
+		if ( !( o instanceof FilenameResource that ) ) return false;
+		if ( !super.equals( o ) ) return false;
+		return Objects.equals( fileName, that.fileName );
+	}
+
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash( super.hashCode(), fileName );
 	}
 }
